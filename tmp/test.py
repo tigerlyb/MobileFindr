f7780fe9f88d95299dc30937e47f738e108be9af
tigerlyb <tigerlyb@gmail.com>
2018-10-02 00:09:22 -0400

    update machine learning example

--- tmp/test.py
+++ tmp/test.py
@@ -1,68 +1,73 @@
 import numpy as np
 import pandas as pd
-import frida
+#import matplotlib.pyplot as plt
 import time
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-
-print(frida.__version__)
-
-data_dir = "./"
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
 
 def load_data(data_dir, train_row):
-    train = pd.read_csv(data_dir + "train.csv")
-    print(train.shape)
-
-    X_train = train.values[0:train_row,1:]
-    y_train = train.values[0:train_row,0]
+    train = pd.read_csv(data_dir + "train.csv").values
+    if train_row > 0:
+        x_train = train[0:train_row, 1:]
+        y_train = train[0:train_row, 0]
+    else:
+        x_train = train[0:, 1:]
+        y_train = train[0:, 0]
     
-    Pred_test = pd.read_csv(data_dir + "test.csv").values
-
-    return X_train, y_train, Pred_test
-
-train_row = 5000
-Origin_X_train, Origin_y_train, Origin_X_test = load_data(data_dir, train_row)
-
-print(Origin_X_train.shape, Origin_y_train.shape, Origin_X_test.shape)
-print(Origin_X_train)
-
-
-X_train,X_vali, y_train, y_vali = train_test_split(Origin_X_train,
-                                                   Origin_y_train,
-                                                   test_size = 0.2,
-                                                   random_state = 0)
-
-print(X_train.shape, X_vali.shape, y_train.shape, y_vali.shape)
-
-ans_k = 0
-
-k_range = range(1, 8)
+    pred_test = pd.read_csv(data_dir + "test.csv").values
+    
+    return x_train, y_train, pred_test
+    
+data_dir = "./"
+train_row = -1 # -1 means train all data, otherwise train specific number of rows
+k_range = range(1, 21)
 scores = []
 
+origin_x_train, origin_y_train, origin_x_test = load_data(data_dir, train_row)
+
+x_train, x_valid, y_train, y_valid = train_test_split(origin_x_train, origin_y_train, test_size = 0.2, random_state = 0)
+
+print("train data: x = %s, y = %s validation data: x = %s, y = %s\n" % (x_train.shape, y_train.shape, x_valid.shape, y_valid.shape))
+
+
 for k in k_range:
-    print("k = " + str(k) + " begin ")
+    print("k = %s begin: " % str(k))
     start = time.time()
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(X_train,y_train)
-    y_pred = knn.predict(X_vali)
-    accuracy = accuracy_score(y_vali,y_pred)
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(x_train, y_train)
+    
+    y_train_pred = knn.predict(x_valid)
+    
+    accuracy = accuracy_score(y_valid, y_train_pred)
     scores.append(accuracy)
     end = time.time()
-    print(classification_report(y_vali, y_pred))  
-    print(confusion_matrix(y_vali, y_pred))  
     
-    print("Complete time: " + str(end-start) + " Secs.")
+    #print(classification_report(y_valid, y_pred))
+    #print(confusion_matrix(y_valid, y_pred))
+    print("accuracy = %s" % str(accuracy))
+    print("training time: %s second(s)\n" % str(end - start))
 
-print (scores)
+score_max = max(scores)    
+k_highest = scores.index(score_max) + 1
+print("total accuracy scores: %s" % scores)
+print("highest accuracy: k = %s, accuracy = %s\n" % (k_highest, score_max))
 
-k = 3
+"""
+plt.plot(k_range, scores)
+plt.xlabel("Value of K")
+plt.ylabel("Accuracy")
+plt.show()
+"""
 
-knn = KNeighborsClassifier(n_neighbors=k)
-knn.fit(Origin_X_train,Origin_y_train)
-y_pred = knn.predict(Origin_X_test[:300])
+start = time.time()
+knn = KNeighborsClassifier(n_neighbors = k_highest)
+knn.fit(origin_x_train, origin_y_train)
+y_test_pred = knn.predict(origin_x_test)
+end = time.time()
 
-print(len(y_pred))
+print("number of recognized images: " + str(len(y_test_pred)))
+print("complete time: %s second(s)\n" % str(end - start))
 
-# save submission to csv
-pd.DataFrame({"ImageId": list(range(1,len(y_pred)+1)),"Label": y_pred}).to_csv('Digit_Recogniser_Result.csv', index=False,header=True)
+pd.DataFrame({"ImageId": list(range(1,len(y_test_pred)+1)),"Label": y_test_pred}).to_csv('Digit_Recogniser_Result.csv', index=False,header=True)
+print("Digit_Recogniser_Result.csv is generated.")
